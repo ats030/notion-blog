193213ffac17ccfc582b258b717c3774a18c1a3c
ats030 <ats030@gmail.com>
2020-07-05 15:53:02 +0900

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,22 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/ats030',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
+    link: 'https://github.com/ats030',
   },
-  {
-    Comp: LinkedIn,
-    alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
-  },
+  #{
+  #  Comp: LinkedIn,
+  #  alt: 'linkedin icon',
+  #  link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+  #},
   {
     Comp: Envelope,
     alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'mailto:ats030@gmail.com',
   },
 ]
 
